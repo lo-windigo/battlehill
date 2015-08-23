09cc834ea44c76ef77f7ecb9dfd142c472a35330
Windigo <jacob@fragdev.com>
2015-08-23 16:34:05 -0700

    Create about page w. content from previous website

---
+++ about.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('lib/template.php');
+
+$title = 'About the brewery';
+
+Top($title);
+?>
+		<h1 class="content-title"><?php echo $title; ?></h1>
+		<p>
+			As the owners and brewers of Battle Hill Brewing Company, we are 
+excited to join the local brewing scene as the area’s newest Farm Brewery.  </p>
+		<p>
+Like many of our peers, we started as home brewers.  As time went by we asked:  
+What if..?   So we began to consider doing this as a career; after all, what 
+could be better than getting paid to do something you love!
+		</p>
+		<p>
+Around the same time as the New York State Farm Brewery Bill, we got serious 
+about the idea of a brewery.  Plans were made, numbers crunched and a business 
+plan drawn up…things evolved and time went by and we kept moving forward.
+		</p>
+		<p>
+Early in 2014 the planets lined up, Battle Hill Brewing Company was formed, we 
+found a home in a cool 19th century building in historic Fort Ann - and the rest 
+is history.  </p>
+		<p>
+Our vision is to create and share our ales 7 barrels at a time, using fresh 
+local ingredients and our refined recipes and brewing practices. Washington 
+County, NY has a long  and varied agricultural history and we’re honored to be 
+part of that, and to hopefully play a part in the resurgence of the area. We 
+think the craft beer and wine industry is a great fit for the county, and we’re 
+very excited to be part of this community.  It’s likely that soon the whole farm 
+to glass experience can be had without leaving our county! 
+		</p>
+		<p>
+			We look forward to innovating , brewing and keeping it local. We 
+think our beers will make you happy…. come see us,  have a pint and take some 
+home to enjoy!
+		</p>
+<?php
+Bottom();
+?>
