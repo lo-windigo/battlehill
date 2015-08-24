8d4bbf933ef5beabc5250556ca7c50d44d39667e
Windigo <jacob@fragdev.com>
2015-08-23 17:10:51 -0700

    Beer page created, index page tweaked

---
+++ beer.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once('lib/template.php');
+
+$title = 'Local craft beers';
+
+Top($title);
+?>
+		<h1 class="content-title"><?php echo $title; ?></h1>
+		<p>
+			In addition to this selection, we will be pouring seasonal beer.
+			Please call the taproom, or check our
+			<a href="http://www.facebook.com/battlehillbrewing">Facebook</a> page
+			for our latest seasonal offerings!
+		</p>
+		<h2 class="beer-name">Grains of Liberty</h2>
+		<p>
+			An all grain and honey session ale brewed in conjunction with area
+			farmers using fresh local ingredients. Pale straw in color. Hints
+			of rye, wheat, oats, corn, honey and spicy flavor profile.
+		</p>
+		<h2 class="beer-name">Podunk Pale Ale</h2>
+		<p>
+			A pale ale light gold in color. Floral evergreen hop aroma. Toasted
+			malts to start followed by moderate hop bitterness with a
+			refreshing smooth finish.
+		</p>
+		<h2 class="beer-name">War Whoop IPA</h2>
+		<p>
+			This beer shouts American piney and citrus hops! Golden in color.
+			Strong hop aroma. Malt taste in the forefront followed by west
+			coast hop flavors. Finishes dry encouraging another sip.
+		</p>
+		<h2 class="beer-name">Burgoyne’s Red</h2>
+		<p>
+			The British are coming! Amber in color. Citrus hops and grain on
+			the nose. English malts and caramel are  balanced by citrus hop
+			flavors and bitterness.
+		</p>
+		<h2 class="beer-name">Cocoa Porter</h2>
+		<p>
+			For the chocolate lover. Dark chocolate in color with a light
+			roasted grain aroma. A generous amount of cocoa nibs are used in
+			the brewing process with enough hops for bitterness to keep it
+			from being overly sweet or cloying. 
+		</p>
+<?php
+Bottom();
+?>
