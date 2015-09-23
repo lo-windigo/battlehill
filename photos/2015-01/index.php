4ac66a873cde13d97313d6c1ffe1d9834ded1b9d
Windigo <jacob@fragdev.com>
2015-09-23 07:44:28 -0700

    Added January 2015 photo gallery

---
+++ photos/2015-01/index.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/lib/template.php';
+
+define('TITLE', 'January 2015');
+
+Top(TITLE);
+?>
+		<h1 class="content-title"><?php echo TITLE; ?></h1>
+		<p>
+			Take a virtual tour of the brewery, from construction to the present
+			day. Get a look behind-the-scenes at the inner workings, and the
+			brewing process that makes Battle Hill Brewing Company special.
+		</p>
+		<ul class="gallery">
+			<li>
+				<a href="01.jpeg"><img src="01_t.jpg" alt=
+					"A snowy image of the exterior ramp and side entrance"></a>
+			</li>
+			<li>
+				<a href="02.jpeg"><img src="02_t.jpg" alt=
+					"The construction of the bar, with supports and wiring"></a>
+			</li>
+			<li>
+				<a href="03.jpeg"><img src="03_t.jpg" alt=
+					"A stairwell and unfinished wall"></a>
+			</li>
+			<li>
+				<a href="04.jpeg"><img src="04_t.jpg" alt=
+					"The front of the unfinished bar, with a bare brick wall in the background"></a>
+			</li>
+			<li>
+				<a href="05.jpeg"><img src="05_t.jpg" alt=
+					"A corner with exposed wiring and supports, and a filled-in doorway"></a>
+			</li>
+			<li>
+				<a href="06.jpeg"><img src="06_t.jpg" alt=
+					"Two people working on the dining room, with construction materials and tools"></a>
+			</li>
+			<li>
+				<a href="07.jpeg"><img src="07_t.jpg" alt=
+					"Another angle of the unfinished dining room"></a>
+			</li>
+			<li>
+				<a href="08.jpeg"><img src="08_t.jpg" alt=
+					"The front of the Battle Hill building"></a>
+			</li>
+			<li>
+				<a href="09.jpeg"><img src="09_t.jpg" alt=
+					"The upstairs, with a partially finished wall and empty light fixtures"></a>
+			</li>
+			<li>
+				<a href="10.jpeg"><img src="10_t.jpg" alt=
+					"Another angle of the upstairs, with windows leaning against the wall"></a>
+			</li>
+			<li>
+				<a href="11.jpeg"><img src="11_t.jpg" alt=
+					"The countertops from behind"></a>
+			</li>
+			<li>
+				<a href="12.jpeg"><img src="12_t.jpg" alt=
+					"The buildings' stone foundation with PVC piping and hanging lights"></a>
+			</li>
+			<li>
+				<a href="13.jpeg"><img src="13_t.jpg" alt=
+					"The corner of the basement and foundation"></a>
+			</li>
+			<li>
+				<a href="14.jpeg"><img src="14_t.jpg" alt=
+					"Jim and Davis standing in front of a radial arm saw"></a>
+			</li>
+			<li>
+				<a href="15.jpeg"><img src="15_t.jpg" alt=
+					"The deck, missing railings in some spots, covered in snow"></a>
+			</li>
+		</ul>
+<?php
+Bottom();
+?>
