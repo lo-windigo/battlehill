de1c2d9e1456e4d639bc6d142b4ee3128b6cd1dc
Windigo <jacob@fragdev.com>
2015-09-22 20:41:31 -0700

    Change image paths to correspond to the new folder

--- photos/opening/index.php
+++ photos/opening/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'lib/template.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/lib/template.php';
 
 define('TITLE', 'Photographs');
 
@@ -14,55 +14,55 @@ Top(TITLE);
 		</p>
 		<ul class="gallery">
 			<li>
-				<a href="/photos/opening/1.jpg"><img src="/photos/opening/1_t.jpg"
+				<a href="1.jpg"><img src="1_t.jpg"
 					alt="Brewery equipment"></a>
 			</li>
 <?php
 //			<li>
-//				<a href="/photos/opening/2.jpg"><img src="/photos/opening/2_t.jpg"
+//				<a href="2.jpg"><img src="2_t.jpg"
 //					alt="The front of the brewery building"></a>
 //			</li>
 ?>
 			<li>
-				<a href="/photos/opening/3.jpg"><img src="/photos/opening/3_t.jpg"
+				<a href="3.jpg"><img src="3_t.jpg"
 					alt="A banner that reads battle hill brewing company; on tap soon"></a>
 			</li>
 			<li>
-				<a href="/photos/opening/4.jpg"><img src="/photos/opening/4_t.jpg"
+				<a href="4.jpg"><img src="4_t.jpg"
 					alt="The deck and ramp on the side of the brewery"></a>
 			</li>
 			<li>
-				<a href="/photos/opening/5.jpg"><img src="/photos/opening/5_t.jpg"
+				<a href="5.jpg"><img src="5_t.jpg"
 					alt="The bar inside the brewery"></a>
 			</li>
 			<li>
-				<a href="/photos/opening/6.jpg"><img src="/photos/opening/6_t.jpg"
+				<a href="6.jpg"><img src="6_t.jpg"
 					alt="Upstairs dining room with several tables and a framed American flag"></a>
 			</li>
 			<li>
-				<a href="/photos/opening/7.jpg"><img src="/photos/opening/7_t.jpg"
+				<a href="7.jpg"><img src="7_t.jpg"
 					alt="The merchandise counter, with tshirts, growlers and hats"></a>
 			</li>
 			<li>
-				<a href="/photos/opening/8.jpg"><img src="/photos/opening/8_t.jpg"
+				<a href="8.jpg"><img src="8_t.jpg"
 					alt="The doorway leading to the patio, looking outside"></a>
 			</li>
 <?php
 //			<li>
-//				<a href="/photos/opening/9.jpg"><img src="/photos/opening/9_t.jpg"
+//				<a href="9.jpg"><img src="9_t.jpg"
 //					alt="The restroom, a clean room with towels, handrail and a sink"></a>
 //					</li>
 ?>
 			<li>
-				<a href="/photos/opening/10.jpg"><img src="/photos/opening/10_t.jpg"
+				<a href="10.jpg"><img src="10_t.jpg"
 					alt="A row of kegs behind-the-scenes, hooked up to the taps with red tubing"></a>
 			</li>
 			<li>
-				<a href="/photos/opening/11.jpg"><img src="/photos/opening/11_t.jpg"
+				<a href="11.jpg"><img src="11_t.jpg"
 					alt="The patio with black metal outdoor chairs and tables"></a>
 			</li>
 			<li>
-				<a href="/photos/opening/12.jpg"><img src="/photos/opening/12_t.jpg"
+				<a href="12.jpg"><img src="12_t.jpg"
 					alt="A decorative gate stating 'May the hinges of friendship never grow rusty'"></a>
 			</li>
 		</ul>
