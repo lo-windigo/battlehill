ccfa0853bba9490538945bac9e87ebc99882a2e3
Windigo <jacob@fragdev.com>
2015-10-23 19:48:12 -0700

    First gallery index

---
+++ photos.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/lib/template.php';
+
+define('TITLE', 'January 2015');
+
+Top(TITLE);
+?>
+		<h1 class="content-title"><?php echo TITLE; ?></h1>
+		<p>
+			The very earliest images of Battle Hill Brewing Co; 
+		</p>
+		<ul class="galleries">
+			<li>
+				<a href="photos/2015-01/"><img src="photos/2015-01/14_t.jpg" alt=
+					""></a><br>
+				<a href="photos/2015-01/">January 2015</a>
+			</li>
+		</ul>
+<?php
+Bottom();
+?>
