887e79737f3a91fc9d15f283b565de5637a7e39e
Windigo <jacob@fragdev.com>
2015-10-23 19:48:47 -0700

    September 2015 pictures

---
+++ photos/2015-09/index.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/lib/template.php';
+
+define('TITLE', 'Photo Galleries');
+
+Top(TITLE);
+?>
+		<h1 class="content-title"><?php echo TITLE; ?></h1>
+		<p>
+			Take a virtual tour of the brewery, from construction to the present
+			day. Get a look behind-the-scenes at the inner workings, and the
+			brewing process that makes Battle Hill Brewing Company special.
+		</p>
+		<ul class="gallery">
+			<li>
+				<a href="01.jpeg"><img src="01_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="02.jpeg"><img src="02_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="03.jpeg"><img src="03_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="04.jpeg"><img src="04_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="05.jpeg"><img src="05_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="06.jpeg"><img src="06_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="07.jpeg"><img src="07_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="08.jpeg"><img src="08_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="09.jpeg"><img src="09_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="10.jpeg"><img src="10_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="11.jpeg"><img src="11_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="12.jpeg"><img src="12_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="13.jpeg"><img src="13_t.jpg" alt=
+					""></a>
+			</li>
+			<li>
+				<a href="14.jpeg"><img src="14_t.jpg" alt=
+					""></a>
+			</li>
+		</ul>
+<?php
+Bottom();
+?>
