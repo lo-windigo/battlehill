6745072218566aadf4ac1978e3d8aa3153e37d72
Windigo <jacob@fragdev.com>
2016-05-12 00:39:53 +0000

    Food menu

---
+++ menu.php
@@ -0,0 +1,202 @@
+<?php
+
+require_once 'lib/template.php';
+
+$title = 'Food Menu';
+
+Top($title);
+?>
+		<h1 class="content-title"><?php echo $title; ?></h1>
+		<p>
+			In addition to our delicious beer selection, our brewery also
+			offers several tasty food choices. We also have a beautiful
+			<a href="docs/menu.pdf" type="application/pdf">printable menu</a>
+			available.
+		</p>
+		<h2>Starters</h2>
+		<ul>
+			<li class="menu-item">
+				<h3>Farmers Platter</h3>
+				<p>
+					Cheese curds from Argyle Cheese, lightly breaded and fried.
+					Assorted cheeses from Crowley Cheese and Sugar Loaf Cheese,
+					seasonal sausage, pickled vegetables, our spent grain bread,
+					and homemade seasonal compote or jelly.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Onion Rings</h3>
+				<p>
+					Hand cut Vidalia onions dipped in our own beer
+					batter blend, fried to a golden brown. Served with our
+					house parmesan peppercorn dressing.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Loaded Potato Chips</h3>
+				<p>
+					Our house made chips topped with sour cream,
+					scallions and bacon served warm and fresh.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Soft Pretzels</h3>
+				<p>
+					Warm fresh pretzels served with our beer cheese, honey
+					mustard, and maple honey dips. 
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Wings</h3>
+				<p>
+					Wings with the Battle Hill touch!
+				</p>
+				<ul>
+					<li><strong>Brew Wings:</strong> Marinated in our rub then fried and tossed with our special beer sauce.</li>
+					<li><strong>BBQ Wings:</strong> A mild house made BBQ sauce with citrus flavors.</li>
+					<li><strong>Mild or Medium Wings:</strong> A mild or medium wing sauce with our own touch.</li>
+					<li><strong>Hot Wings:</strong> A traditional hot wing sauce with our own touch.</li>
+				</ul>
+			</li>
+		</ul>
+		<h2>Salads</h2>
+		<ul>
+			<li class="menu-item">
+				<h3>House Salad</h3>
+				<p>
+					Baby lettuces, tomatoes, shredded carrot, chopped hard boiled
+					eggs and shredded Cheddar.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Chef Salad</h3>
+				<p>
+					Baby lettuces, cucumbers, tomatoes, shredded carrot,
+					hardboiled eggs, oven roasted turkey, ham, and roast
+					beef, with a slice of house made spent grain bread.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Mandarin Orange Salad</h3>
+				<p>
+					Baby lettuces, shredded carrots, mandarin oranges,
+					sliced almonds, dried cranberries, fried wonton strips,
+					and a house made mandarin orange dressing.
+					Grilled chicken can be added.
+				</p>
+			</li>
+		</ul>
+		<h2>Soup and Chili</h2>
+		<p>
+			Please ask your server for our <strong>soup or chili</strong>
+			choice of the day as they are made fresh each morning (all soups
+			are gluten free).
+		</p>
+		<ul>
+			<li class="menu-item">
+				<h3>Battle hill bowl &amp; wedge</h3>
+				<p>
+					Your choice of a half sandwich: grilled cheese, turkey, roast beef, or
+					ham with lettuce and tomato and a cup of our homemade soup or
+					chili of the day (wraps not included).
+				</p>
+			</li>
+		</ul>
+		<h2>Sandwiches</h2>
+		<ul>
+			<li class="menu-item">
+				<h3>Fort Anne pub club</h3>
+				<p>
+					Homemade, toasted cranberry spent grain bread,
+					slow roasted natural turkey, sliced granny smith
+					apples, Applewood bacon and mild cheddar cheese,
+					topped with our own blend of local homemade honey
+					mustard dressing
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Grilled chicken breast</h3>
+				<p>
+					Grass fed chicken breast, marinated and seasoned
+					with our special blend of beer and fresh herbs.
+					Grilled and topped with Arugula Aoli, baby lettuces,
+					tomato, and onion.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Toasted Cheese</h3>
+				<p>
+					Toasted cheese sandwich with tomato, onion, and
+					local cheddar.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Ham and brie melt</h3>
+				<p>
+					Slow roasted natural ham grilled on our spent grain
+					bread with Brie, Applewood bacon, and our unique
+					honey maple dressing.
+				</p>
+			</li>
+		</ul>
+		<h2>Burgers</h2>
+		<ul>
+			<li class="menu-item">
+				<h3>Patriot burger</h3>
+				<p>
+					An all American grass fed beef patty cooked to perfection,
+					topped with locally grown baby lettuce, tomato, onion, and
+					American cheese on a toasted house made roll.
+				</p>
+			</li>
+			<li class="menu-item">
+				<h3>Battle burger</h3>
+				<p>
+					A grass fed beef patty seasoned in house. Topped with smoked gouda, caramelized
+					onion, mushrooms, and our homemade steak sauce on a toasted homemade roll.
+				</p>
+			</li>
+		</ul>
+		<h2>Beer brats &amp; kraut</h2>
+		<p>
+			German Brats cooked in Fort Ann Amber then grilled and placed on a bed of sautéed
+			beer kraut with apples and onions. Served with our Daily Vegetable.
+		</p>
+		<h2>Little Troopers</h2>
+		<ul>
+			<li class="menu-item">Macaroni &amp; cheese</li>
+			<li class="menu-item">Grilled cheese</li>
+			<li class="menu-item">Trooper burger</li>
+			<li class="menu-item">Chicken strips</li>
+		</ul>
+		<p>
+			All meals served with two sides, a drink, &amp; dessert.
+		</p>
+		<p>
+			<strong>Sides:</strong> Applesauce, French Fries, Mandarin Oranges
+		</p>
+		<p>
+			<strong>Drinks:</strong> Milk, Juice Box
+		</p>
+		<p>
+			<strong>Dessert:</strong> Ice Cream (vanilla, chocolate, or strawberry) topped with
+			chocolate syrup, whipped cream and a cherry.
+		</p>
+		<h2>Beverages</h2>
+		<ul>
+			<li class="menu-item">
+				<strong>Boylan’s:</strong> Cola, Diet Cola, Orange, Birch Beer
+			</li>
+			<li class="menu-item">
+				<strong>Chef Choice:</strong> Root Beer, Spicy Ginger
+			</li>
+			<li class="menu-item">
+				<strong>Other Choices:</strong> Sprite, Bottled water, Tea, Coffee and Lemonade
+			</li>
+		</ul>
+		<p>
+			Ask your server about today’s Dessert selection
+		</p>
+<?php
+Bottom();
+?>
