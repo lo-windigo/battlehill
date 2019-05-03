eee5ba827fac591471e73112892b81e3484c2b27
Windigo <jacob@fragdev.com>
2019-05-02 20:29:19 -0700

    Small fix to hours for Summer 2019

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ if($today < $vacation_end_date) {
 					alt="Find Battle Hill Brewing Company on Facebook"></a>
 			</p>
 			<p>
-				Wednesday, Thursday, and Friday: 11:30AM &ndash; 8PM<br>
-				Saturday and Sunday: 11:30AM &ndash; 9PM
+				Wednesday, Thursday, and Sunday: 11:30AM &ndash; 8PM<br>
+				Friday and Saturday: 11:30AM &ndash; 9PM
 			</p>
 		</div>
 		<div class="home-intro">
